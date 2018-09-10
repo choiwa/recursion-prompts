d63deed560d0747af7d225d361b44f209c85ef46
Joyce Cho <joyjoycho@gmail.com>
2018-09-10 00:40:12 -0700

    complete sumBelowl

--- src/recursion.js
+++ src/recursion.js
@@ -40,7 +40,7 @@ var arraySum = function(array) {
     if (Array.isArray(current)) {
       sum += arraySum(current);
     } else {
-          sum += current;
+      sum += current;
     }
    }
 
@@ -68,7 +68,18 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
-  
+  if (n === 0) {
+    return 0;
+  }
+
+  if (n > 0) {
+    return n - 1 + sumBelow(n - 1);
+  }
+
+  if (n < 0) {
+    return n + 1 + sumBelow(n + 1);
+  }
+
 };
 
 // 6. Get the integers within a range (x, y).
