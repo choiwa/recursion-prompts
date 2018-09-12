7099e7ba2d143162f51cf8b50fe405f85470e183
Joyce Cho <joyjoycho@gmail.com>
2018-09-11 19:27:58 -0700

    complete flatten with two ways of recursions

--- src/recursion.js
+++ src/recursion.js
@@ -149,6 +149,17 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  var string = string.toLowerCase();
+
+  if (string.length === 1 || string.length === 0) {
+    return true;
+  }
+
+  if (string[0] === string[string.length - 1]) {
+    return palindrome(string.substring(1, string.length - 1));
+  } else {
+    return false;
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
@@ -157,6 +168,21 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+  if (x === 1) {
+    return 1;
+  }
+
+  if (x === 0) {
+    return 0;
+  }
+
+  if (x > y) {
+    return modulo(x - y, y);
+  }
+
+  if (x < y) {
+    return modulo(x - y, y);
+  }
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
@@ -279,6 +305,38 @@ var nestedEvenSum = function(obj) {
 // 30. Flatten an array containing nested arrays.
 // flatten([1,[2],[3,[[4]]],5]); // [1,2,3,4,5]
 var flatten = function(array) {
+  // define an array
+  var arr = [];
+  // loop thru the array
+  for (let i = 0; i < array.length; i++) {
+    var current = array[i];
+
+    if (Array.isArray(current)) {
+      arr = arr.concat(flatten(current));
+    } else {
+      arr.push(current);
+    }
+  }
+
+  return arr;
+    // if current element is an array
+      // recursion
+    // if current element is number
+      // return push element to array
+
+  // var recursion = (array) => {
+  //   for (let i = 0; i < array.length; i++) {
+  //     var current = array[i];
+  //
+  //     if (Array.isArray(current)) {
+  //       recursion(current);
+  //     } else {
+  //       flattenArray.push(current);
+  //     }
+  //   }
+  // }
+  //
+  // recursion(array);
 };
 
 // 31. Given a string, return an object containing tallies of each letter.
