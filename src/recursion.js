18b5aae5b33603f2df2be2dbe4cc594f263ca171
Joyce Cho <joyjoycho@gmail.com>
2018-10-11 14:03:59 -0700

    compress

--- src/recursion.js
+++ src/recursion.js
@@ -508,6 +508,17 @@ var letterTally = function(str, obj = {}) {
 // compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
 // compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
 var compress = function(list) {
+  var compressedArr = [];
+
+  if (!list.length) {
+    return compressedArr;
+  }
+
+  if (list[0] !== list[1]) {
+    compressedArr.push(list[0]);
+  }
+
+  return compressedArr.concat(compress(list.slice(1)));
 };
 
 // 33. Augument every element in a list with a new value where each element is an array
