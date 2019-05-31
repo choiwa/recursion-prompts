a7d1a48cb7d96086ce04f422bf2a068c5f861a46
choiwa <joyjoycho@gmail.com>
2019-05-31 12:02:24 -0700

    complete first n fibonacci

--- src/recursion.js
+++ src/recursion.js
@@ -378,15 +378,17 @@ var replaceKeysInObj = function (obj, oldKey, newKey) {
 // fibonacci(5); // [0,1,1,2,3,5]
 // Note: The 0 is not counted.
 var fibonacci = function (n) {
-  if (output === undefined) {
-    let output = [0, 1];
+  if (n <= 0) {
+    return null;
   }
 
-  if (output.length === n) {
-    return output;
+  if (n === 1) {
+    return [0, 1];
   }
 
+  let previousArr = fibonacci(n - 1);
 
+  return previousArr.concat(previousArr[previousArr.length - 1] + previousArr[previousArr.length - 2]);
 };
 
 // 26. Return the Fibonacci number located at index n of the Fibonacci sequence.
